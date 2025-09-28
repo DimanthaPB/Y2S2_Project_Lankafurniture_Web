f037768935b8276270ca80967a9e234b742aa8de
Pasindu B. Dimantha <pasindubhagya2001@gmail.com>
2025-09-28 22:28:52 +0530

    Revert "Admin/User Login & Employee/Salary Management"
    This reverts commit 3c71018a259bb0cae1335a57c228eb5109449207.

--- backend/utils/numberToWords.js
+++
@@ -1,8 +0,0 @@
-const numberToWords = require('number-to-words');
-
-const convertToWords = (num) => {
-  if (!num) return '';
-  return numberToWords.toWords(num).replace(/\b\w/g, char => char.toUpperCase());
-};
-
-module.exports = convertToWords;
--- frontend/src/utils/formatDate.js
+++
@@ -1,8 +0,0 @@
-export default function formatDate(date) {
-  if (!date) return "";
-  try {
-    return new Date(date).toLocaleString();
-  } catch {
-    return String(date);
-  }
-}
